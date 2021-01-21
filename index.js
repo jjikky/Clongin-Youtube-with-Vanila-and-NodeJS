de6fb1167ad40ef4bac3ff6a9238c2b1a96547a0
jikky5418 <kok5418@naver.com>
2021-01-21 20:23:13 +0900

    Express core: Middlewares part Two

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 import express from "express"; //const express = require("express");
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
+
 const app = express();
 
 const PORT = 4000;
@@ -7,22 +12,22 @@ const handleListening = (req,res) => console.log(`Listening on : http://localhos
 
 const handleHome = (req,res) => res.send("Hi from Home!!");
 
-const handleProfile =  (req, res) => res.send("You are on my profile"); // 바벨을 이용한 신코드 (js의 arrow function)
-// 바벨은 신코드(ES6~)를 이용할 수 있게 해주며, 브라우저에서 동작할 때 신코드를 다시 구 코드로 변환해준다.
-/* 구코드
+const handleProfile =  (req, res) => res.send("You are on my profile"); // New code using babel (arrow function of js)
+// Babel allows you to use the new code (ES6~), and converts the new code back to the old code when running in the browser.
+/* This is old code
 function handleProfile(req,res){
     res.send("profile");
 } */
 
-const betweenHome = (req,res,next) => {
-    console.log("between");
-    next();  // 다음 미들웨어 호출
-};
-
-
+app.use(cookieParser());   // How the server understands the cookies it receives from users
+app.use(bodyParser.json());     // How the server understands the data it receives from users. Using .json() to understand not only form but also json
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(morgan("dev"));   // ex : GET/profile 304 - - 2.873ms
+app.use(helmet());        // For security
+// arrived at the route, after going through all the above codes
 
 app.get('/', handleHome);
 
 app.get("/profile", handleProfile);
-app.use(betweenHome);
+
 app.listen(PORT, handleListening);
