0ab6877c3939549da7b8e9ab359775c577355093
jikky5418 <kok5418@naver.com>
2021-01-22 02:54:03 +0900

    MVC pattern part Three

--- routers/userRouter.js
+++ routers/userRouter.js
@@ -1,5 +1,17 @@
 import express from "express";
+import routes from "../routes";
+import {
+    users,
+    userDetail,
+    editProfile,
+    changePassword
+  } from "../controllers/userController";
 
 const userRouter = express.Router();
+
+userRouter.get(routes.users, users);
+userRouter.get(routes.userDetail, userDetail);
+userRouter.get(routes.editProfile, editProfile);
+userRouter.get(routes.changePassword, changePassword);
 
 export default userRouter; // export dafult한건 이 파일을 export 하는 거고 변수앞에 export만 하는건 오직 그 변수만 export한다는것
