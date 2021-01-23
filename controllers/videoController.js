f0c3e325003bca00372dd0a74ec330926eecdb6b
jikky5418 <kok5418@naver.com>
2021-01-24 02:45:21 +0900

    Search Controller

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,7 +1,13 @@
 //if you give the name of the template file as an argument to the render() function,
 //this function will show after finding the template file with the file name home makeover pug in the folder
 export const home = (req, res) => res.render("home", {pageTitle: 'Home'}); 
-export const search = (req, res) => res.render("search", {pageTitle: 'Search'});
+export const search = (req, res) => {
+    const {
+        query: {term: searchingBy}
+    } = req;
+    //const searchingBy = req.query.term;
+    res.render("search", {pageTitle: 'Search', searchingBy});
+}
 export const upload = (req, res) => res.render("upload", {pageTitle: 'Upload'});
 export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: 'Video Detail'});
 export const editVideo = (req, res) => res.render("editVideo", {pageTitle: 'Edit Video'});
