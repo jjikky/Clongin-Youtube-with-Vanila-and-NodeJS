5f124d483c33e0da3d0b6b1aca67002f79669d67
jikky5418 <kok5418@naver.com>
2021-01-24 02:15:01 +0900

    Template Variables in Pug

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,8 +1,8 @@
 //if you give the name of the template file as an argument to the render() function,
 //this function will show after finding the template file with the file name home makeover pug in the folder
-export const home = (req, res) => res.render("home"); 
-export const search = (req, res) => res.render("search");
-export const upload = (req, res) => res.render("upload");
-export const videoDetail = (req, res) => res.render("videoDetail");
-export const editVideo = (req, res) => res.render("editVideo");
-export const deleteVideo = (req, res) => res.render("deleteVideo");
+export const home = (req, res) => res.render("home", {pageTitle: 'Home'}); 
+export const search = (req, res) => res.render("search", {pageTitle: 'Search'});
+export const upload = (req, res) => res.render("upload", {pageTitle: 'Upload'});
+export const videoDetail = (req, res) => res.render("videoDetail", {pageTitle: 'Video Detail'});
+export const editVideo = (req, res) => res.render("editVideo", {pageTitle: 'Edit Video'});
+export const deleteVideo = (req, res) => res.render("deleteVideo", {pageTitle: 'Delete Video'});
