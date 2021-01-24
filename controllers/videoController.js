c2b890fb45f4d1c0700d48ba20b70bebabee6a3d
jikky5418 <kok5418@naver.com>
2021-01-25 00:53:32 +0900

    Home Controller Finished

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,9 +1,16 @@
 import routes from "../routes";
+import Video from "../models/Video";
 
 //if you give the name of the template file as an argument to the render() function,
 //this function will show after finding the template file with the file name home makeover pug in the folder
-export const home = (req, res) => {
-    res.render("home", {pageTitle: 'Home', videos});
+export const home = async (req, res) => {  
+    try{
+        const videos = await Video.find({});   // Using async and await to make this part wait until the end, If you don't make it wait, it will take care of this and move on to the next.
+        res.render("home", {pageTitle: 'Home', videos});
+    } catch(error){
+        console.log(error);
+        res.render("home", {pageTitle: 'Home', videos: []});
+    }
 } 
 export const search = (req, res) => {
     const {
