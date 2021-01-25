d94fec3a2644ad245e2f2775f4bf90ce3cbfd811
jikky5418 <kok5418@naver.com>
2021-01-26 03:35:43 +0900

    Login the User and Session on Express

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
+import passport from "passport";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
-import routes from "./routes";
+
+import "./passport";
 
 const app = express();
 
@@ -23,6 +27,15 @@ app.use(cookieParser());   // How the server understands the cookies it receives
 app.use(bodyParser.json());     // How the server understands the data it receives from users. Using .json() to understand not only form but also json
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev"));   // ex : GET/profile 304 - - 2.873ms
+app.use(
+    session({
+        secret: process.env.COOKIE_SECRET,    // For encrypt Session ID
+        resave: true,
+        saveUninitialized: false
+    })
+)
+app.use(passport.initialize());  // get cookie from cookieParser, passport initialize and find User corresponding to cookie
+app.use(passport.session());    // then passport makes user it finds object of request(req.user)
 app.use(localsMiddleware);
 // arrived at the route, after going through all the above codes
 
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,10 +1,11 @@
+import passport from "passport";
 import routes from "../routes";
 import User from "../models/User";
 
 export const getJoin = (req, res) => {
     res.render("join", { pageTitle: "Join" });
 }
-export const postJoin = async (req, res) => {
+export const postJoin = async (req, res, next) => {
     const {
         body: { name, email, password, password2 }
     } = req;
@@ -18,17 +19,18 @@ export const postJoin = async (req, res) => {
                 email
             });
             await User.register(user, password);
+            next();
         } catch (error) {
             console.log(error);
+            res.redirect(routes.home);
         }
-        res.redirect(routes.home);
     }
 };
 export const getLogin = (req, res) => res.render("login");
-export const postLogin = (req, res) => {
-    // To Do : Process Log out
-    res.redirect(routes.home);
-};
+export const postLogin = passport.authenticate('local', {   // 'local' is the name of Strategy that we installed
+    failureRedirect: routes.login,
+    successRedirect: routes.home
+});
 
 export const logout = (req, res) => {
 
