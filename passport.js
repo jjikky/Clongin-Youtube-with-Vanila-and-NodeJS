4ba2c94c4cbbe65d286f0ad336923d2c05c53428
jikky5418 <kok5418@naver.com>
2021-01-26 20:48:36 +0900

    6.7(code completion but not work) > skip to 7

--- passport.js
+++ passport.js
@@ -1,9 +1,22 @@
 import passport from "passport";
-import User from "./models/User"
+import GithubStrategy from "passport-github";
+import User from "./models/User";
+import { githubLoginCallback } from "./controllers/userController";
+import routes from "./routes";
 
 
 passport.use(User.createStrategy());          // username과 password를 쓰는 인증방식을 사용하기 위함(이미 구성이 된 passport-local의 LocalStrategy를 생성)
 
+passport.use(
+    new GithubStrategy(
+        {
+            clientID: process.env.GH_ID,
+            clientSecret: process.env.GH_SECRET,
+            callbackURL: `http://localhost:4000${routes.githubCallback}`
+        },
+        githubLoginCallback
+    )
+);
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
--- routes.js
+++ routes.js
@@ -15,6 +15,11 @@ const UPLOAD = "/upload";
 const VIDEO_DETAIL = "/:id";
 const EDIT_VIDEO = "/:id/edit";
 const DELETE_VIDEO = "/:id/delete";
+
+// Github
+const GITHUB = "/auth/github";
+const GITHUB_CALLBACK = "/auth/github/callback";
+
 const routes = {
   home: HOME,
   join: JOIN,
@@ -53,7 +58,9 @@ const routes = {
     } else {
       return DELETE_VIDEO;
     }
-  }
+  },
+  gitHub: GITHUB,
+  githubCallback: GITHUB_CALLBACK
 };
 
 export default routes; 
